3cb8f5cc001fb614b7221c46f18b92932262ccfc
23-Pravallika <learngain23@gmail.com>
2024-02-23 19:36:29 +0530

    AutoPush

--- rotue53.tf
+++ rotue53.tf
@@ -1,6 +1,6 @@
-resource "aws_route53_record" "record" {
+resource "aws_route53_record" "tf-record" {
   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
-  name    = "${var.COMPONENT}-${var.ENV}"
+  name    = "${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"
   type    = "A"
   ttl     = 10
   records = var.LB_TYPE == "internal" ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
