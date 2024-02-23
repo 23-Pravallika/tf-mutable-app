2a23734ad6ba347f3198334bf86ac6b42e92fc88
23-Pravallika <learngain23@gmail.com>
2024-02-23 18:31:15 +0530

    AutoPush

--- tg.tf
+++ tg.tf
@@ -15,22 +15,22 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
 }
 
 
-## Adds a rule inside the listener  ( private )
 resource "aws_lb_listener_rule" "app_rule" {
-#  count        =  var.LB_TYPE == "internal" ? 1 : 0  
+  count        =  var.LB_TYPE == "internal" ? 1 : 0
+
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-  priority     = 1
+  priority     = 100
 
   action {
     type             = "forward"
-    target_group_arn = aws_lb_target_group.alb_app_tg.arn
+    target_group_arn = aws_lb_target_group.app.arn
   }
+
   condition {
     host_header {
       values = ["${var.COMPONENT}-${var.ENV}.${data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_NAME}"]
     }
   }
 }
-
 
 
