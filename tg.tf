c1983883cffc107dc58ff68b248c1ba14bccd417
23-Pravallika <learngain23@gmail.com>
2024-02-23 20:46:36 +0530

    AutoPush

--- tg.tf
+++ tg.tf
@@ -16,9 +16,9 @@ resource "aws_lb_target_group_attachment" "attach_instances" {
 
 
 resource "aws_lb_listener_rule" "app_rule" {
-  count        =  var.LB_TYPE == "internal" ? 1 : 0
+#   count        =  var.LB_TYPE == "internal" ? 1 : 0
 
-  listener_arn = element(data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN, count.index)
+  listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
   priority     = 100
 
   action {
