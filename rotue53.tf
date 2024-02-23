88f15a629357bb3ad76ef70ae638bf0dae0597fa
23-Pravallika <learngain23@gmail.com>
2024-02-23 17:52:53 +0530

    AutoPush

--- rotue53.tf
+++ rotue53.tf
@@ -1,10 +1,10 @@
-resource "aws_route53_record" "record" {
-  zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
-  name    = "www.example.com"
-  type    = "A"
-  ttl     = 10
-  records = var.LB_TYPE == "internal" ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
-}
+# resource "aws_route53_record" "record" {
+#   zone_id = data.terraform_remote_state.vpc.outputs.PRIVATE_HOSTED_ZONE_ID
+#   name    = "www.example.com"
+#   type    = "A"
+#   ttl     = 10
+#   records = var.LB_TYPE == "internal" ? [data.terraform_remote_state.alb.outputs.PRIVATE_ALB_ADDRESS] : [data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ADDRESS]
+# }
 
 
 
