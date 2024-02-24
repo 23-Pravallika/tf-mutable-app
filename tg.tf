752e2d1069079948b7003459c8f347d97170f086
23-Pravallika <learngain23@gmail.com>
2024-02-24 11:36:25 +0530

    AutoPush

--- tg.tf
+++ tg.tf
@@ -39,15 +39,17 @@ resource "random_integer" "priority" {
   max = 50000
 }
 
-#
-# resource "aws_lb_listener" "public" {
-#   load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
-#   port              = 80
-#   protocol          = "HTTPS"
+# Public ALB Listerner
+# Creates a listener to the target group
+resource "aws_lb_listener" "public" {
+  count             =  var.LB_TYPE == "internal" ? 0 : 1  
+  load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+  port              = 80
+  protocol          = "HTTP"
  
-#   default_action {
-#     type             = "forward"
-#     target_group_arn = aws_lb_target_group.alb_app_tg.arn
-#   }
-# }
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.alb_app_tg.arn
+  }
+}
 
