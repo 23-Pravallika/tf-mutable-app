28b8a12feef9ce5fd5d010bf4ee1f21f634a04a8
23-Pravallika <learngain23@gmail.com>
2024-02-24 10:26:09 +0530

    AutoPush

--- tg.tf
+++ tg.tf
@@ -19,7 +19,7 @@ resource "aws_lb_listener_rule" "app_rule" {
 #   count        =  var.LB_TYPE == "internal" ? 1 : 0
 
   listener_arn = data.terraform_remote_state.alb.outputs.PRIVATE_LISTENER_ARN
-  priority     = 100
+  priority     = random_integer.priority.result
 
   action {
     type             = "forward"
@@ -33,4 +33,21 @@ resource "aws_lb_listener_rule" "app_rule" {
   }
 }
 
+# Generates a random integar
+resource "random_integer" "priority" {
+  min = 1
+  max = 50000
+}
+
+#
+# resource "aws_lb_listener" "public" {
+#   load_balancer_arn = data.terraform_remote_state.alb.outputs.PUBLIC_ALB_ARN
+#   port              = 80
+#   protocol          = "HTTPS"
+ 
+#   default_action {
+#     type             = "forward"
+#     target_group_arn = aws_lb_target_group.alb_app_tg.arn
+#   }
+# }
 
